3d966bbc0a8e949c434f815d042de8b8fd132865
Adam Mazur <mazut@o2.pl>
2023-05-15 08:23:55 +0200

    Added try/catch block but it quits from the loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,22 +5,23 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
+        
         NumbersFromUser numbersFromUser = new NumbersFromUser();
+        numbersFromUser.getNumbersFromUser();
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Write a number bigger than 0 and smaller than 100: ");
-            numbersFromUser.setNumberFromUser(scanner.nextInt());
-            if (numbersFromUser.getNumberFromUser()!=0) {
-                numbersFromUser.setOfNumbersFromUser.add(numbersFromUser.getNumberFromUser());
-            }
-            if (numbersFromUser.setOfNumbersFromUser.size() != i+1) {
-                System.out.println("Given Number must be unique! Try again!");
-                i--;
 
-            }
-        }
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println("Write a number bigger than 0 and smaller than 100: ");
+//            numbersFromUser.setNumberFromUser(scanner.nextInt());
+//            if (numbersFromUser.getNumberFromUser()!=0) {
+//                numbersFromUser.setOfNumbersFromUser.add(numbersFromUser.getNumberFromUser());
+//            }
+//            if (numbersFromUser.setOfNumbersFromUser.size() != i+1) {
+//                System.out.println("Given Number must be unique! Try again!");
+//                i--;
+//
+//            }
+//        }
         for (int number :
                 numbersFromUser.setOfNumbersFromUser) {
             System.out.println(number);
--- src/main/java/org/example/NumbersFromUser.java
+++ src/main/java/org/example/NumbersFromUser.java
@@ -1,24 +1,51 @@
 package org.example;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class NumbersFromUser {
 
-    private int numberFromUser;
-
     Set<Integer> setOfNumbersFromUser = new TreeSet<>();
+    int numberFromUser;
 
-    public int getNumberFromUser() {
-        return numberFromUser;
+    public void getNumbersFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Write a number bigger than 0 and smaller than 100: ");
+            try {
+                numberFromUser = scanner.nextInt();
+            } catch (InputMismatchException exception) {
+                System.out.println("Sorry, but only integers are allowed here!");
+                return;
+            }
+            if (numberFromUser <= 0 || numberFromUser >= 100) {
+                System.out.println("Given number is out of scope! Try again!");
+                i--;
+                continue;
+            }
+
+            setOfNumbersFromUser.add(numberFromUser);
+            System.out.println("Your number has been added");
+
+
+            if (setOfNumbersFromUser.size() != i + 1) {
+                System.out.println("Given Number must be unique! Sorry, but your number has been deleted. Try again!");
+                i--;
+            }
+        }
     }
 
-    public void setNumberFromUser(int numberFromUser) {
-        if (numberFromUser<=0 || numberFromUser>=100) {
-            System.out.println("Given number is out of scope");
-        }
-        else {
-            this.numberFromUser = numberFromUser;
-        }
-    }
+//    public int getNumberFromUser() {
+//        return numberFromUser;
+//    }
+//
+//    public void setNumberFromUser(int numberFromUser) {
+//        if (numberFromUser <= 0 || numberFromUser >= 100) {
+//            System.out.println("Given number is out of scope");
+//        } else {
+//            this.numberFromUser = numberFromUser;
+//        }
+//    }
 }
