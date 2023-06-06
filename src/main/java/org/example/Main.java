f7397c7fea475c876142fb84b7daac83d9720323
Adam Mazur <mazut@o2.pl>
2023-06-06 19:03:10 +0200

    Started with tests2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,7 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-//        String name = args[0];
-//        String surname = args[1];
-//        System.out.println(name);
-//        System.out.println(surname);
+
         Starter starter = new Starter();
         String resultMessage = starter.starter();
         System.out.println(resultMessage);
--- src/test/java/org/example/LottoGameTest.java
+++ src/test/java/org/example/LottoGameTest.java
@@ -3,31 +3,33 @@ package org.example;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.MockSettings;
 import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
 
 import java.util.*;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class LottoGameTest {
 
-//NumbersRetriever numbersRetrieverMock = mock(NumbersRetriever.class);
-    NumbersRetriever numbersRetrieverMock = new NumbersRetriever();
-//Drawing drawingMock = mock(Drawing.class);
-    Drawing drawingMock = new Drawing();
-
-@Test
-    void win_six_of_six(){
+    NumbersRetriever numbersRetrieverMock = mock(NumbersRetriever.class);
 
 
-    LottoGame lottoGame = new LottoGame();
-    GameResult realResult = lottoGame.playGame();
-    when(numbersRetrieverMock.retrieve()).thenReturn(new TreeSet<>(Arrays.asList(1,2,3,4,5,6)));
-    when(drawingMock.makeListOfConsecutiveNumbers()).thenReturn(new ArrayList<>(List.of(1,2,3,4,5,6)));
-    GameResult expectedResult = new GameResult("You guessed 6 numbers out of 6, you won :-) :-D :-D :-)");
-    Assertions.assertThat(realResult.getMessage()).isEqualTo(expectedResult.getMessage());
 
-}
+    Drawing drawingMock = mock(Drawing.class);
+
+    @Test
+    void win_six_of_six() {
+
+        
+        LottoGame lottoGame = new LottoGame();
+        GameResult realResult = lottoGame.playGame();
+        when(numbersRetrieverMock.retrieve()).thenReturn(new TreeSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        when(drawingMock.makeListOfConsecutiveNumbers()).thenReturn(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
+        GameResult expectedResult = new GameResult("You guessed 6 numbers out of 6, you won :-) :-D :-D :-)");
+        Assertions.assertThat(realResult.getMessage()).isEqualTo(expectedResult.getMessage());
+
+    }
 
 }
